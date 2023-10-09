a35394eb387c72da21483741c77d09514384dc6a
vcc <linsky519@gmail.com>
2023-10-08 22:22:21 -0600

    pirate, starting on mud

---
+++ Module4/englishToPirate.py
@@ -0,0 +1,33 @@
+import string
+englishPirateDict = {"hello":"ahoy", "to":"ta", "was":"be", "cheat":"hornswaggle", "cheating":"hornswaggle'n",
+"toilet":"head", "hi":"yo-ho-ho", "man":"matey","pardon":"avast", "excuse":"arrrgh", "yes":"aye", "my":"me", 
+"friend":"matey", "sir":"matey", "miss":"comely wench", "stranger":"scurvy dog", "officer":"foul blaggart", 
+"where":"whar", "is":"be", "for":"fer", "are":"be", "am":"be", "the":"th", "going":"goin", "you":"ye", 
+"your":"yer", "tell":"be tellin", "know":"be knowin", "far":"many leagues", "old":"barnacle-covered", 
+"attractive":"comely", "happy":"grog-filled", "quickly":"smartly", "nearby":"broadside", "restroom":"head", 
+"restaurant":"galley", "hotel":"fleabag inn", "bar":"Skull & Scuppers", "mall":"market", "bank":"buried treasure", 
+"die":"visit Davey Jones Locker", "died":"visited Davey Jones Locker", "kill":"keel-haul", "killed":"keel-hauled", 
+"sleep":"take a caulk", "stupid":"addled", "after":"aft", "stop":"belay", "nonsense":"bilge", "ocean":"briny deep", 
+"go":"get ye", "song":"shanty", "money":"doubloons", "drunk":"three sheets to the wind", "food":"grub", "nose":"prow", 
+"leave":"weigh anchor", "cheat":"hornswaggle", "forward":"fore", "child":"sprog", "children":"sprogs", "sailor":"swab", 
+"lean":"careen", "find":"come across", "mother":"dear ol mum, bless her black soul", 
+"mom":"dear ol mum, bless her black soul", "drink":"barrel o rum", "of":"o", "there":"thar", "my":"me", "mine":"me", 
+"gun":"cannon", "monkey":"tailed imp", "expert":"old smartly", "flag":"Jolly Roger", "dad":"capn", "teacher":"wise sage", 
+"phone":"cursed device", "computer":"magic box", "speak":"parley", "person":"landlubber", "people":"landlubbers", 
+"sir":"matey", "hotel":"fleabag", "student":"swabbie", "boy":"matey", "professor":"foul blaggart", "restaurant":"galley", 
+"students":"swabbies", "bathroom":"head"}
+
+english = input("Enter your words in English: ").lower()
+
+clean = ''.join(ch for ch in english if ch not in set(string.punctuation))
+
+englishList = clean.split()
+pirate = ""
+
+for word in englishList:
+  if word in englishPirateDict:
+    pirate += englishPirateDict[word] + " "
+  else:
+    pirate += word + " "
+
+print(pirate)
--- Module4/mud.py
+++ Module4/mud.py
@@ -4,6 +4,9 @@
 # The player sets aside any 2s and 5s, and throws the remaining dice. Again, if any 2s or 5s are thrown, those dice are stuck in the mud.  The remaining dice are added to the score.
 # Continue in this way until all the dice are “Stuck in the Mud.” After the score is totaled, play passes to the left.
 # Write a program that will simulate one round of the Stuck in the Mud game.  Create an array for the dice and populate it with random numbers to simulate the dice.  Sum the values that are not equal to 2 and 5.  If the user chooses to roll again, roll only the values that are not stuck in the mud.
+import random
 
 print("Stuck in the Mud")
+
+dices = []
 
