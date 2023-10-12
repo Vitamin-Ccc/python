10eb24985458312267d083b4112622a56fce93fa
vcc <linsky519@gmail.com>
2023-10-12 16:32:47 -0600

    dice roll assignment done.

--- Module4/diceRollSimulator.py
+++ Module4/diceRollSimulator.py
@@ -3,9 +3,25 @@
 # Allow the user to choose how many times the “dice” will be thrown. Then, once the dice have been thrown the specified number of times, print out how many times each number was thrown.
 
 # For 10 bonus points print a histogram (using the * character) that shows the total percentage each number was rolled. Each * will represent 1% of the total rolls.
+import random
 
+rollDict = { }
+for i in range(1, 7):
+  for j in range(1, 7):
+    if i+j not in rollDict:
+      rollDict[i+j] = 0
+      
 print("Welcome to the dice throwing simulator!")
 times = int(input("How many dice rolls would you like to simulate? "))
-rollDict = { }
 print("DICE ROLLING SIMULATION RESULTS")
 print("Total number of rolls =", times)
+
+for i in range(times):
+  sum = random.randrange(1, 7) + random.randrange(1,7)
+  if sum in rollDict:
+    rollDict[sum] += 1
+
+for i in rollDict:
+  print(str(i)+":", rollDict[i])
+
+print("Thank you for using the dice throwing simulator. Goodbye!")
